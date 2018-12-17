89a0ce3705106a166db7d15a2099ff0ab1081068
atoris1192 <atoris1192@gmail.com>
2018-12-18 00:47:28 +0900

    add insertMany deleteMany

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const url = 'mongodb://localhost:27017/myproject'
+const dbName = 'myproject'
+const client = new MongoClient(url)
+const { 
+  insertManyDocuments,
+  deleteManyDocuments,
+  findDocuments,
+
+} = require('./collectionMethod.js')
+
+client.connect( err => {
+    assert.equal(null, err)
+    console.log("Connected successfully to Server")
+    const db = client.db(dbName)
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'mongodb' })
+  });
+  router.get('/deleteMany', (req, res) => {
+    console.log(req.query)
+    deleteManyDocuments(db, result => {
+      console.log(result.result)
+    })
+    res.send('delete done')
+  })
+  router.get('/insertMany', (req, res) => {
+    console.log(req.query)
+    insertManyDocuments(db, result => {
+      console.log(result.result)
+    })
+    res.send('insert done')
+  })
+  router.get('/find', (req, res) => {
+    console.log(req.query);
+    findDocuments(db, docs => {
+      console.log(docs)
+    })
+    
+  })
+})
 
 module.exports = router;
