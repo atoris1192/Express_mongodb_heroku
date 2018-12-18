1a17ea71d19419ca05c97dd069c1924fbae17d7e
atoris1192 <atoris1192@gmail.com>
2018-12-18 12:00:49 +0900

    indexjsをasyncに書き直し

---
+++ config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  URL             : 'mongodb://localhost:27017/myproject',
+  DB_NAME         : 'myproject',
+  COLLECTION_NAME : 'documents',
+}
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
+// const { URL, DB_NAME, COLLECTION_NAME } = require('../config.js')
 
-const url = 'mongodb://localhost:27017/myproject'
-const dbName = 'myproject'
-const client = new MongoClient(url)
+const data = {
+  URL             : 'mongodb://localhost:27017/myproject',
+  DB_NAME         : 'myproject',
+  COLLECTION_NAME : 'documents',
+}
+// const client = new MongoClient(data.URL, { useNewUrlParser: true })
 const { 
   insertManyDocuments,
   deleteManyDocuments,
@@ -13,35 +17,73 @@ const {
 
 } = require('./collectionMethod.js')
 
-client.connect( err => {
-    assert.equal(null, err)
-    console.log("Connected successfully to Server")
-    const db = client.db(dbName)
-  /* GET home page. */
-  router.get('/', function(req, res, next) {
-    res.render('index', { title: 'mongodb' })
-  });
-  router.get('/deleteMany', (req, res) => {
-    console.log(req.query)
-    deleteManyDocuments(db, result => {
-      console.log(result.result)
+const main = async () => {
+  const client = new MongoClient(data.URL, { useNewUrlParser: true })
+  try {
+    await client.connect()
+    console.info("Connected successfully to Server")
+    const db = client.db(data.DB_NAME)
+    let r 
+
+    router.get('/', function(req, res, next) {
+      res.render('index', { title: 'mongodb' })
+    });
+    router.get('/deleteMany', (req, res) => {
+      deleteManyDocuments(db)
+        .then(r => console.log('DeleteMany result: ', r.result))
+        .catch(err => console.error('DeleteMany result: ', err))
+      res.send('delete done')
     })
-    res.send('delete done')
-  })
-  router.get('/insertMany', (req, res) => {
-    console.log(req.query)
-    insertManyDocuments(db, result => {
-      console.log(result.result)
+    router.get('/insertMany', (req, res) => {
+      insertManyDocuments(db)
+      console.log('InsertMany: ', r)
+      res.send('insert done')
     })
-    res.send('insert done')
-  })
-  router.get('/find', (req, res) => {
-    console.log(req.query);
-    findDocuments(db, docs => {
-      console.log(docs)
+    router.get('/find', (req, res) => {
+      findDocuments(db)
+        .then(docs => console.log('docs: ', docs))
+        .catch(err => console.error(err))
+      res.end()
     })
+
+
+  } catch(err) {
+    console.error(err.stack)
+    client.close()
+  }
+}
+
+main()
+
+// client.connect( err => {
+//     assert.equal(null, err)
+//     console.log("Connected successfully to Server")
+//     const db = client.db(data.DB_NAME)
+//   /* GET home page. */
+//   router.get('/', function(req, res, next) {
+//     res.render('index', { title: 'mongodb' })
+//   });
+//   router.get('/deleteMany', (req, res) => {
+//     console.log(req.query)
+//     deleteManyDocuments(db, result => {
+//       console.log(result.result)
+//     })
+//     res.send('delete done')
+//   })
+//   router.get('/insertMany', (req, res) => {
+//     console.log(req.query)
+//     insertManyDocuments(db, result => {
+//       console.log(result.result)
+//     })
+//     res.send('insert done')
+//   })
+//   router.get('/find', (req, res) => {
+//     console.log(req.query);
+//     findDocuments(db, docs => {
+//       console.log(docs)
+//     })
     
-  })
-})
+//   })
+// })
 
 module.exports = router;
